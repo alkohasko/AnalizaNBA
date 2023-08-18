6faf280418a441ed29bc1efe4d83fd9ef2c56400
Aljaz <aljaz.flus@gmail.com>
2023-08-18 13:29:22 +0200

    spreminjanje grafa

--- ureditev.py
+++ ureditev.py
@@ -4,12 +4,18 @@ import matplotlib.pyplot as plt
 def graf_metov():
     sezone = pd.read_csv("C:/FAKS/UVP/AnalizaNBA-projektna/obdelani_podatki/sezone.csv", index_col="Sezona")
     x = sezone.index
-    y = sezone["Število metov"]
+    y1 = sezone["Število metov"]
+    y2 = sezone["Število zadetih metov"]
     
     plt.figure(figsize=(10, 6))
-    plt.plot(x, y)
+    plt.plot(x, y1, label = "Število metov")
+    plt.plot(x, y2, label = "Število zadetih metov")
     plt.xlabel("Sezona")
     plt.ylabel("Število metov")
     plt.title("Število metov skozi sezone")
-    plt.xticks([x[0], x[-1]], rotation=45)
+    plt.xticks([x[0]] + list(x[:-1:10]) + [x[-1]], rotation=45)
+    plt.gca().invert_xaxis()
+    plt.legend()
     plt.show()
+
+pokazi_tabelo()
